adbda0311031e54184b662f361cb1d3767920911
kris34 <lalov.kr@gmail.com>
2025-08-31 17:13:27 +0300

    remove /setup

--- app/page.js
+++ app/page.js
@@ -8,10 +8,6 @@ export default async function Home() {
   // Check if the post table exists
   const tableExists = await checkPostTableExists();
 
-  // If the post table doesn't exist, redirect to setup page
-  if (!tableExists) {
-    redirect('/setup');
-  }
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center py-24 px-8">
