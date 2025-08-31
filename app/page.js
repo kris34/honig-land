f9f955c0dff12e19025657a38c9d6c3076c69150
kris34 <lalov.kr@gmail.com>
2025-08-31 17:14:48 +0300

    remove: unused vars

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
 export const dynamic = 'force-dynamic'; // This disables SSG and ISR
 
-import { redirect } from 'next/navigation';
-import { checkPostTableExists } from '@/lib/db-utils';
 import HomepageBanner from './components/HomepageBanner'
 
 export default async function Home() {
   // Check if the post table exists
-  const tableExists = await checkPostTableExists();
-
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center py-24 px-8">
